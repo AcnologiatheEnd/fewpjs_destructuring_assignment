adf998ffa035c3757b39fee35ecf0b8a0cc22fa7
Winston  Chung <18.chung.winston@ctkprep.org>
2020-04-26 17:25:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const [Bessie, , Dolly, Babe, Little] = farmAnimals.split(' ')
 const [black_and_white, , black, pink, ] = farmAnimals.split(' ')
 const [red, orange, yellow, green, blue, indigo, violet] = colors
 const [r, o, y, g, b, , v] = colors
-const [indg] = colors[5]
+
 
 const [, , , , , indg] = colors
 // Objects
